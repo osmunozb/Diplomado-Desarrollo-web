d73813e117019bc00c9630d2b901825865a20cf7
osmunozb <102986907+osmunozb@users.noreply.github.com>
2022-07-21 19:11:29 -0400

    Add files via upload

---
+++ 4-Desarrollo Web Con Bootstrap Y Javascript/proyecto_01/assets/js/script.js
@@ -0,0 +1,52 @@
+//Popovers
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+
+
+// Alerta
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('liveAlertBtn')
+
+function alert(message, type) {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+  alertPlaceholder.append(wrapper)
+}
+
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', function () {
+    alert('Se ha producido un error. En caso de un crimen en progreso recuerda llamar a la policía. ', 'danger')
+  })
+}
+
+
+
+// Toast (al interior del offcanvas)
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
+
+
+
+//Placeholder Noticias del momento
+
+//tiempo en milisegundos para la función
+const myTimeout = setTimeout(miFuncion, 5000);
+
+//pasos de la función 
+function miFuncion() {
+document.getElementById("contenedor-placeholder").innerHTML = document.getElementById("card-normal").innerHTML
+}
+
+
+
